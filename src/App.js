0bab261b5a8d981f547216b2a2e5f75449242828
Kuba <kuba.nowakowski@sabre.com>
2023-07-27 23:21:40 +0200

    init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          FIRST DEPLOY ON AWS
+          Learn React
         </a>
       </header>
     </div>
